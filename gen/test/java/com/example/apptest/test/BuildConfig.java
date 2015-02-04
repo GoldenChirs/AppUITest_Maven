be74011f5cf38f35843b113758fbe0678f1f95bd
Deen Gao <deengao@gmail.com>
2015-02-04 10:26:26 +0800

    AppUITest

---
+++ gen/test/java/com/example/apptest/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package test.java.com.example.apptest.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ target/generated-sources/r/test/java/com/example/apptest/test/BuildConfig.java
@@ -0,0 +1,5 @@
+package test.java.com.example.apptest.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = true;
+}
